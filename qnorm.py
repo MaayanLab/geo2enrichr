7f34b975d711021cecf06ae443c9c2a6e5c29bc8
Greg <ggundersen@gmail.com>
2014-12-15 12:42:50 -0500

    Quantile normalization working

--- qnorm.py
+++ qnorm.py
@@ -7,70 +7,58 @@ __contact__ = "avi.maayan@mssm.edu"
 
 import numpy as np
 
-import pdb
-
-import operator
-
 
 def qnorm(A, B):
-	"""
-	"""
+	"""Performs quantile normalization on two arrays of arrays.
 
-	# TODO: Condense this code while retaining legibility! It is important not
-	# to create half a dozen large numpy array objects.
+	Quantile normalization is a 4 step algorithm to make two or more
+	distributions identical in statistical properties. Below is a
+	visualization:
+
+	Original	Ranked		Averaged	Re-ordered
+	2,20		2,8			5,5			5,13
+	6,8		 	6,20		13,13		13,5
+
+	Read more here: http://en.wikipedia.org/wiki/Quantile_normalization
+	"""
 
 	# axis=0 means that an operation runs against columns rather than rows.
 
-	# 1. Original:
-	# Stack them, i.e. concatenate the rows "vertically".
-	O = np.vstack((A, B))
+	# Store height so we can un-concatenate arrays later.
+	h = len(A)
 
-	# 2. Ranked:
-	# First argsort gets the order. Second argsort gets the rank. Keep this
-	# copy for later. See http://stackoverflow.com/a/6266510/1830334.
-	ordered = np.argsort(O, axis=0)
-	R = np.argsort(ordered, axis=0)
+	# 1. Create matrix, i.e. concatenate the rows vertically.
+	O = np.vstack((np.array(A), np.array(B)))	
 
-	# 3. Sorted by rank:
-	E = np.sort(O, axis=0)
-	C = E.copy()
+	# 2. Sorted by rank.
+	M = np.sort(O, axis=0)
 
-	# 4. Averaged:
-	D = np.mean(C, axis=1)
+	# 3. Averaged.
+	# axis=1 indicates that the means are calculated across rows, not columns.
+	D = np.mean(M, axis=1)
 	for i, avg in enumerate(D):
-		C[i].fill(avg)
+		M[i].fill(float(avg))
 
-	F = np.zeros((5,3))
-	for i, row in enumerate(C):
+	# 4a. Ranked by index:
+	# First argsort gets the order. Second argsort gets the rank. See
+	# http://stackoverflow.com/a/6266510/1830334.
+	I = np.argsort(np.argsort(O, axis=0), axis=0)
+
+	# 4b. Move values back to their original locations.
+	for i, row in enumerate(M):
 		for j, val in enumerate(row):
-			#pdb.set_trace()
-			row = R[i][j]
-			F[row][j] = C[i][j]
+			row, col = np.where(I==i)
+			O[row, col] = M[i][j]
 
+	A, B = np.vsplit(O, h)
+	return (A.tolist(), B.tolist())
 
-
-	# http://stackoverflow.com/a/6835668/1830334
-	print ''
-	pdb.set_trace()
-	__print( C[np.argsort(C[:,0])[1]] )
-
-	#pdb.set_trace()
-	return (A, B)
-
-
-def __print(M):
-	for r in M:
-		s = ''
-		for c in r:
-			s += str(c)
-		print s 
 
 if __name__ == '__main__':
 	A = [[2,4,4],
 		[ 5,4,14]]
 
 	B = [[4,6,8],
-		[ 3,5,8],
-		[ 3,3,9]]
+		[ 3,5,8]]
 
-	qnorm(A, B)
+	A, B = qnorm(A, B)
