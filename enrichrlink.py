d217ab21bd1fe360a08356ea6fad0c976daa2664
Greg <ggundersen@gmail.com>
2014-12-30 18:39:39 -0500

    Added functionality for descriptions

--- enrichrlink.py
+++ enrichrlink.py
@@ -14,27 +14,29 @@ import poster
 from files import GeneFile
 
 
-def get_link(filename):
+def get_link(filename, description):
 	gene_str = GeneFile(filename).stringify_contents()
-	link = _post_and_build_link(gene_str)
+	link = _post_and_build_link(gene_str, description)
 	return link
 
 
-def _post_and_build_link(genes_str, cluster_info=''):
+def _post_and_build_link(genes_str, description):
 	""" POST a gene list to Enrichr server and get a stable link to the
 	enriched data.
 	"""
 
-	BASE_URL = "http://amp.pharm.mssm.edu/Enrichr/"
+	BASE_URL = 'http://amp.pharm.mssm.edu/Enrichr/'
 
 	opener = poster.streaminghttp.register_openers()
 	opener.add_handler(urllib2.HTTPCookieProcessor(cookielib.CookieJar()))
 
 	params = {
 		'list': genes_str,
-		'description': cluster_info
+		'description': description
 	}
 
+	import pdb
+	pdb.set_trace()
 	datagen, headers = poster.encode.multipart_encode(params)
 	request = urllib2.Request(BASE_URL + 'enrich', datagen, headers)
 	urllib2.urlopen(request)
