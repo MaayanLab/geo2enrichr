570aa896ba1de687be676214dbd33a2094eff614
Greg <ggundersen@gmail.com>
2015-01-20 17:58:58 -0500

    Auto generated files

--- extension/g2e.js
+++ extension/g2e.js
@@ -11,9 +11,14 @@ var Comm = function(events, notifier, SERVER) {
 	var fileForDownload = [];
 
 	// We need to make back-to-back AJAX requests to get the relevant data.
-	// Querying GEO's esearch endpoint with an accession number returns some JSON with an ID.
-	// Then we query GEO's esummary endpoint with the ID.
-	// Finally, we fire an event so that any modules that want this data can subscribe.
+	// The steps:
+	// 1. Query GEO's esearch endpoint with an accession number returns some
+	//    JSON with an ID.
+	// 2. Query GEO's esummary endpoint with the ID.
+	// 3. Fire an event so that any modules that want this data can subscribe.
+	//
+	// This is *not* an IIFE because the scraper must first collect the accession
+	// from the URL.
 	var fetchMetadata = function(accession) {
 		var EUTILS_BASE = 'http://eutils.ncbi.nlm.nih.gov/entrez/eutils/',
 			DB = 'db=gds',
@@ -36,96 +41,7 @@ var Comm = function(events, notifier, SERVER) {
 			}
 		});
 	};
-
-	// This is the workhorse function that chains together multiple AJX requests to the back-end.
-	var downloadDiffExp = function(userInput) {
-		function dlgeo() {
-			var endpoint = 'dlgeo?',
-				qs = $.param({
-					accession: userInput.accession,
-					organism: userInput.organism,
-					platform: userInput.platform,
-					method: userInput.method,
-					cell: userInput.cell,
-					perturbation: userInput.perturbation,
-					gene: userInput.gene
-				});
-
-			return $.ajax({
-				url: SERVER + endpoint + qs,
-				type: 'POST',
-				success: function(data) {
-				    debugger;
-					if (data.status === 'error') {
-						errorHandler(data);
-						return;
-					}
-					notifier.log('GEO files were downloaded');
-					notifier.log(data);
-					events.fire('progressBar');
-				}
-			});
-		}
-
-		function diffexp(dlgeoData) {
-			var endpoint = 'diffexp?',
-				qs = $.param({
-					// This is the SOFT file, not the gene file.
-					filename: dlgeoData.filename,
-					platform: userInput.platform,
-					method: userInput.method,
-					control: userInput.control.join('-'),
-					experimental: userInput.experimental.join('-')
-				});
-
-			return $.ajax({
-				url: SERVER + endpoint + qs,
-				type: 'POST',
-				success: function(data) {
-					if (data.status === 'error') {
-						errorHandler(data);
-						return;
-					}
-					var DIR = 'static/genes/';
-					notifier.log('GEO files were differentially expressed');
-					notifier.log(data);
-					events.fire('progressBar');
-					events.fire('dataDiffExped', {
-						'up': SERVER + DIR + data.up,
-						'down': SERVER + DIR + data.down
-					});
-				}
-			});
-		}
-
-		function enrichr(diffExpData) {
-			var endpoint = 'enrichr?',
-				qs = $.param({
-					'up': diffExpData.up,
-					'down': diffExpData.down,
-					'combined': diffExpData.combined
-				});
-
-			return $.ajax({
-				url: SERVER + endpoint + qs,
-				type: 'POST',
-				success: function(data) {
-					if (data.status === 'error') {
-						errorHandler(data);
-						return;
-					}
-					notifier.log('Enrichr link was returned');
-					notifier.log(data);
-					events.fire('progressBar');
-					events.fire('genesEnriched', data);
-				}
-			});
-		}
-
-		// Pass in noops so that we do nothing if the promise is not returned.
-		dlgeo().then(diffexp, $.noop).then(enrichr, $.noop);
-	};
-
+	
 	var fetchGeneList = function() {
 		$.ajax({
 			url: 'http://amp.pharm.mssm.edu/Enrichr/json/genemap.json',
@@ -147,6 +63,96 @@ var Comm = function(events, notifier, SERVER) {
 			}
 		});       
 	}();
+
+	var getAjax = function(endpoint, data, callback) {
+        return $.ajax({
+            url: SERVER + endpoint,
+            type: 'POST',
+            data: JSON.stringify(data),
+            contentType: 'application/json;charset=UTF-8',
+            crossDomain: true,
+            success: callback
+        });
+	};
+
+	// This is the workhorse function that chains together multiple AJX requests to the back-end.
+	var downloadDiffExp = function(userInput) {
+		function dlgeo() {
+			var data = {
+                accession: userInput.accession,
+                organism: userInput.organism,
+                platform: userInput.platform,
+                method: userInput.method,
+                cell: userInput.cell,
+                perturbation: userInput.perturbation,
+                gene: userInput.gene
+            };
+			
+			var success = function(data) {
+                if (data.status === 'error') {
+                    errorHandler(data);
+                    return;
+                }
+                notifier.log('GEO files were downloaded');
+                notifier.log(data);
+                events.fire('progressBar');
+            };
+
+			return getAjax('dlgeo', data, success);
+		}
+
+		function diffexp(dlgeoData) {
+			var data = {
+                // This is the SOFT file, not the gene file.
+                filename: dlgeoData.filename,
+                platform: userInput.platform,
+                method: userInput.method,
+                control: userInput.control.join('-'),
+                experimental: userInput.experimental.join('-')
+            };
+
+			var success = function(data) {
+                if (data.status === 'error') {
+                    errorHandler(data);
+                    return;
+                }
+                var DIR = 'static/genes/';
+                notifier.log('GEO files were differentially expressed');
+                notifier.log(data);
+                events.fire('progressBar');
+                events.fire('dataDiffExped', {
+                    'up': SERVER + DIR + data.up,
+                    'down': SERVER + DIR + data.down
+                });
+            };
+
+			return getAjax('diffexp', data, success);
+		}
+
+		function enrichr(diffExpData) {
+			var data = {
+                'up': diffExpData.up,
+                'down': diffExpData.down,
+                'combined': diffExpData.combined
+            };
+
+			var success = function(data) {
+                if (data.status === 'error') {
+                    errorHandler(data);
+                    return;
+                }
+                notifier.log('Enrichr link was returned');
+                notifier.log(data);
+                events.fire('progressBar');
+                events.fire('genesEnriched', data);
+            };
+
+			return getAjax('enrichr', data, success);
+		}
+
+		// Pass in noops so that we do nothing if the promise is not returned.
+		dlgeo().then(diffexp, $.noop).then(enrichr, $.noop);
+	};
 
 	var errorHandler = function(data) {
 		events.fire('requestFailed', data);
