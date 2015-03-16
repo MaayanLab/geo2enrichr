dbe15d8b7f69ae4be1df6d649567940b9ae44a9e
Greg <ggundersen@gmail.com>
2015-03-16 19:05:49 -0400

    Updated docstring.

--- g2e/core/genelist/genelistfilemanager.py
+++ g2e/core/genelist/genelistfilemanager.py
@@ -10,7 +10,7 @@ EXT = '.txt'
 
 
 def write(name, ranked_genes):
-    """Writes the contents of a SoftFile to disk and returns a relative path.
+    """Writes the contents of a GeneList to disk and returns a relative path.
     """
     full_path = BASE_DIR + name + EXT
     with open(full_path, 'w+') as f:
