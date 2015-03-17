b7240cc71c364d801af396800f17da953948e115
Greg <ggundersen@gmail.com>
2015-03-16 20:06:54 -0400

    STABLE EOD: Fixed unit tests.

--- g2e/tests/test_extract_endpoint.py
+++ g2e/tests/test_extract_endpoint.py
@@ -8,7 +8,7 @@ unittest.TestCase.maxDiff = None
 
 
 def get_gene_value(genelist, gene):
-    for gv in genelist:
+    for gv in genelist['ranked_genes']:
         if gv[0] == gene:
             return gv[1]
     return None
@@ -27,23 +27,21 @@ class ExtractEndpoint(unittest.TestCase):
         ))
         self.resp_dict = json.loads(self.resp.data.decode())
 
-    def test_status_code(self):
+    # This should be multiple unit tests but it is annoyingly slow to run.
+    def test_extraction_endpoint(self):
         self.assertEquals(self.resp.status_code, 200)
-
-    def test_json_response(self):
         self.assertTrue('extraction_id' in self.resp_dict)
 
-    def test_get_by_extraction_id(self):
         extraction_id = self.resp_dict['extraction_id']
         resp = self.app.get('/g2e/extract?id=' + str(extraction_id))
         resp_dict = json.loads(resp.data.decode())
 
         self.assertTrue(resp_dict['method'] == 'chdir')
         self.assertTrue(resp_dict['cutoff'] == 500)
-        self.assertTrue(resp_dict['softfile'] == 'GDS5077')
+        self.assertTrue(resp_dict['softfile']['name'] == 'GDS5077')
         self.assertTrue('enrichr_link_up' in resp_dict)
         self.assertTrue('enrichr_link_down' in resp_dict)
-        self.assertTrue('softfile_link' in resp_dict)
+        self.assertTrue('text_file' in resp_dict['softfile'])
         self.assertTrue('genelist' in resp_dict)
 
         genelist = resp_dict['genelist']
