453bd06c42069766440a73bdd5102bc9c0e61fa2
Greg <ggundersen@gmail.com>
2015-03-24 15:30:42 -0400

    RC20150324: GEO2Enrichr 2.0, full stack web application plus half Firefox

--- g2e/web/plugin/lib/main.js
+++ g2e/web/plugin/lib/main.js
@@ -3,6 +3,7 @@ var pageMod = require("sdk/page-mod");
 
 pageMod.PageMod({
     include: "http://www.ncbi.nlm.nih.gov/*",
-    contentScriptFile: [data.url("jquery-1.11.1.min.js"), data.url("jquery-ui.min.js"), data.url("g2e.js"), data.url("main.css")],
-    contentScriptWhen: 'start'
+    contentScriptFile: [data.url("jquery-1.11.1.min.js"), data.url("jquery-ui.min.js"), data.url("g2e.js")],
+    contentStyleFile: data.url("main.css"),
+    contentScriptWhen: 'end'
 });
