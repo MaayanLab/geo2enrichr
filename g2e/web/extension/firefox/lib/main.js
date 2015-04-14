fdf182cb34478b63a7555607655616f2a89136b4
Greg <ggundersen@gmail.com>
2015-04-14 13:14:24 -0400

    Changed to jQuery 11.2 hoping to pass Firefox AMO review

--- g2e/web/extension/firefox/lib/main.js
+++ g2e/web/extension/firefox/lib/main.js
@@ -4,7 +4,7 @@ var self = require("sdk/self");
 
 pageMod.PageMod({
     include: "http://www.ncbi.nlm.nih.gov/*",
-    contentScriptFile: [ self.data.url("jquery-1.11.1.min.js"), self.data.url("jquery-ui.min.js"), self.data.url("g2e.js") ],
+    contentScriptFile: [ self.data.url("jquery-1.11.2.min.js"), self.data.url("jquery-ui.min.js"), self.data.url("g2e.js") ],
     contentStyleFile:  [ self.data.url("main.css"), self.data.url("open-sans.css") ],
     contentScriptWhen: 'end',
     contentScriptOptions: {
