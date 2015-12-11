60b5ba875a74687e1c10e371d56f112a07364e0c
Greg <ggundersen@gmail.com>
2015-12-11 10:10:00 -0500

    RC20151211: Fixed broken Clustergrammer pages, making them similar to PCA pages

--- g2e/endpoints/api/cluster.py
+++ g2e/endpoints/api/cluster.py
@@ -1,7 +1,7 @@
 """API for hierarchical clustering.
 """
 
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 from g2e.core.targetapp import clustergrammer
 from g2e.config import Config
@@ -17,4 +17,9 @@ def perform_hierarchical_clustering(extraction_id):
     """Performs hierarchical clustering on a SOFT file.
     """
     gene_signature = dataaccess.fetch_gene_signature(extraction_id)
-    return clustergrammer.from_soft_file(gene_signature)
+    link = clustergrammer.from_soft_file(gene_signature)
+    link = link + '&preview=true'
+    return render_template('pages/clustergrammer.html',
+                           clustergrammer_link=link,
+                           results_url=Config.RESULTS_PAGE_URL,
+                           extraction_id=gene_signature.extraction_id)
