b2e6aeb97e72478e140b2f2195d3ce37bf4e43cd
Greg <ggundersen@gmail.com>
2015-12-30 13:57:21 -0500

    Removed unnecessary code without changing test

--- tests/test_cluster.py
+++ tests/test_cluster.py
@@ -24,14 +24,10 @@ class TestCluster(unittest.TestCase):
         ))
         post_response = json.loads(temp.data.decode())
         self.extraction_id = post_response['extraction_id']
-        temp = self.app.get('/g2e/api/extract/' + str(self.extraction_id))
-        response = json.loads(temp.data.decode())
-
-        self.direction = 0
-        self.up_gene_list = response['gene_lists'][self.direction]
 
     def testEndpoint(self):
         print(self.extraction_id)
         url = '/g2e/cluster/' + self.extraction_id
+        import pdb; pdb.set_trace()
         response = self.app.get(url)
         self.assertTrue('http://amp.pharm.mssm.edu/clustergrammer/viz/' in response.data)
