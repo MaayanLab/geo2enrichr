644e2b09cf3bfbe6a2c94c58bd8378152d93c098
Greg <ggundersen@gmail.com>
2015-12-30 15:38:33 -0500

    Renamed pages and apis to include suffixes

--- g2e/__init__.py
+++ g2e/__init__.py
@@ -9,7 +9,6 @@ from flask.ext.cors import CORS
 from g2e.config import Config
 from substrate import db
 
-
 app = Flask(__name__, static_url_path='/g2e/static', static_folder='static')
 app.config['SQLALCHEMY_DATABASE_URI'] = Config.SQLALCHEMY_DATABASE_URI
 app.config['SQLALCHEMY_POOL_RECYCLE'] = Config.SQLALCHEMY_POOL_RECYCLE
@@ -29,20 +28,20 @@ else:
 
 # Import these after connecting to the DB.
 from g2e.endpoints.pages.menupages import menu_pages
-from g2e.endpoints.pages.results import results_page
+from g2e.endpoints.pages.resultspage import results_page
 
-from g2e.endpoints.api.extract import extract_api
-from g2e.endpoints.api.genelist import gene_list_api
-from g2e.endpoints.api.pca import pca_api
-from g2e.endpoints.api.cluster import cluster_api
-from g2e.endpoints.api.softfile import soft_file_api
+from g2e.endpoints.api.extractapi import extract_api
+from g2e.endpoints.api.genelistapi import gene_list_api
+from g2e.endpoints.api.pcaapi import pca_api
+from g2e.endpoints.pages.clusterpage import cluster_page
+from g2e.endpoints.api.softfileapi import soft_file_api
 
 from g2e.utils.jinjafilters import jinjafilters
 
 app.register_blueprint(menu_pages)
 app.register_blueprint(results_page)
+app.register_blueprint(cluster_page)
 
-app.register_blueprint(cluster_api)
 app.register_blueprint(extract_api)
 app.register_blueprint(gene_list_api)
 app.register_blueprint(pca_api)
--- g2e/endpoints/pages/clusterpage.py
+++ g2e/endpoints/pages/clusterpage.py
@@ -7,12 +7,12 @@ from g2e.core.targetapp import clustergrammer
 from g2e.config import Config
 from g2e.db import dataaccess
 
-cluster_api = Blueprint('cluster_api',
+cluster_page = Blueprint('cluster_api',
                         __name__,
                         url_prefix='%s/cluster' % Config.BASE_URL)
 
 
-@cluster_api.route('/<extraction_id>', methods=['GET'])
+@cluster_page.route('/<extraction_id>', methods=['GET'])
 def perform_hierarchical_clustering(extraction_id):
     """Performs hierarchical clustering on a SOFT file.
     """
