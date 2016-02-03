4df258cb306986de8faf6fba1e952f6b13b10072
Greg <ggundersen@gmail.com>
2016-02-03 11:04:12 -0500

    Removed unused filters

--- g2e/endpoints/jinjafilters.py
+++ g2e/endpoints/jinjafilters.py
@@ -1,38 +1,15 @@
 """Custom filters for Jinja2 templates.
 """
 
-
 from flask import Blueprint
 import jinja2
-import urllib
-
-from g2e import config
 
 
 jinjafilters = Blueprint('filters', __name__)
 
 
-# String utils
-# ----------------------------------------------------------------------------
-
-@jinja2.contextfilter
-@jinjafilters.app_template_filter('c_urlencode')
-def c_urlencode(context, value):
-    return urllib.quote_plus(value)
-
-
 # Data filters
 # ----------------------------------------------------------------------------
-
-@jinja2.contextfilter
-@jinjafilters.app_template_filter('c_filter_organism')
-def c_filter_organism(context, value):
-    for metadata in value:
-        if metadata.name == 'organism':
-            return metadata.value
-    return None
-
-
 @jinja2.contextfilter
 @jinjafilters.app_template_filter('c_filter_optional_metadata')
 def c_filter_optional_metadata(context, value):
@@ -41,60 +18,21 @@ def c_filter_optional_metadata(context, value):
         if (
             metadata.value == None or
             metadata.value.strip() == '' or
-            #metadata.name == 'organism' or
             metadata.name == 'user_key' or
             metadata.name == 'userKey' or
             metadata.name == 'userEmail' or
-            metadata.name == 'user_email'):
+            metadata.name == 'user_email'
+        ):
             continue
         results.append(metadata)
     return results
 
 
-@jinja2.contextfilter
-@jinjafilters.app_template_filter('c_filter_empty')
-def c_filter_empty(context, value):
-    if not value or value == 'None':
-        return ''
-    return value
-
-
 # URL builders
 # ----------------------------------------------------------------------------
-
-@jinja2.contextfilter
-@jinjafilters.app_template_filter('c_tag_url')
-def c_tag_url(context, value):
-    return '%s/%s' % (config.GENEVA_REPORT_URL, value)
-
-
-@jinja2.contextfilter
-@jinjafilters.app_template_filter('c_results_url')
-def c_results_url(context, value):
-    return '%s/%s' % (config.RESULTS_PAGE_URL, value)
-
-
-@jinja2.contextfilter
-@jinjafilters.app_template_filter('c_metadata_url')
-def c_metadata_url(context, value):
-    return '%s/%s' % (config.GEN3VA_METADATA_URL, value)
-
-
-@jinja2.contextfilter
-@jinjafilters.app_template_filter('c_metadata_value_url')
-def c_metadata_value_url(context, value, name):
-    return '%s/%s/%s' % (config.GEN3VA_METADATA_URL, name, value)
-
-
 @jinja2.contextfilter
 @jinjafilters.app_template_filter('c_geo_url')
 def c_geo_url(context, value):
     if 'GDS' in value:
         return 'http://www.ncbi.nlm.nih.gov/sites/GDSbrowser?acc=' + value
     return 'http://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=' + value
-
-
-@jinja2.contextfilter
-@jinjafilters.app_template_filter('c_gen3va_url')
-def c_gen3va_url(context, value):
-    return config.GEN3VA_URL
