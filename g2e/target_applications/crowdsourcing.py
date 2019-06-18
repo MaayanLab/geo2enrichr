d7c34b7211b10410f90913c4a3fbefc1c857c157
allielarocco <51710320+allielarocco@users.noreply.github.com>
2019-06-18 11:42:50 -0400

    Update crowdsourcing.py
    syntax update

--- g2e/target_applications/crowdsourcing.py
+++ g2e/target_applications/crowdsourcing.py
@@ -58,7 +58,7 @@ def _build_link(tag):
 
 
 def _post(genes, optional_metadata, soft_file, tag):
-    print '\tPOSTing ' + tag.name
+    print('\tPOSTing ' + tag.name)
 
     # If we submit an identical study (same GDS or GSE with the same GSMs),
     # the crowdsourcing app rejects the study. For testing purposes, we want
