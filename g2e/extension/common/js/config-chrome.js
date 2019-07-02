1fc95c9c5a180ad2a1536b8d05e5debd21f7155a
Daniel J. B. Clarke <u8sand@gmail.com>
2019-07-02 17:26:34 -0400

    Use https instead of http

--- g2e/extension/common/js/config-chrome.js
+++ g2e/extension/common/js/config-chrome.js
@@ -1,4 +1,4 @@
 // This file is built by deploy.py in the root directory.
 var DEBUG = false;
-var SERVER = "http://amp.pharm.mssm.edu/g2e/";
+var SERVER = "https://amp.pharm.mssm.edu/g2e/";
 var IMAGE_PATH = "chrome-extension://pcbdeobileclecleblcnadplfcicfjlp/logo-50x50.png";
